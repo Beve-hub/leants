38dab526d146f03bf1a4a857d16aa1baca98c79d
Believe <117173080+Beve-hub@users.noreply.github.com>
2023-09-28 15:47:20 -0600

    today

--- leatr/src/App.js
+++ leatr/src/App.js
@@ -1,16 +1,16 @@
-
-import './App.css';
+import Bind from './component/Bind';
+import Eventbind from './component/Eventbind';
 import Form from './component/Form';
-//import Bind from './component/Bind';
-// import Eventbind from './component/Eventbind';
-// import FunctionClick from './component/FunctionClick';
-// import Greeting from './component/Greeting';
-// import NameList from './component/NameList';
-// import ParentComponent from './component/ParentComponent';
-// import Stae from './component/Stae';
-// import UserGreeting from './component/UserGreeting';
-// import Tipping from './component/practice/Tipping';
-//import StyleSheet from './component/StyleSheet';
+import FunctionClick from './component/FunctionClick';
+import Greeting from './component/Greeting';
+import LifecycleA from './component/LifecycleA';
+import NameList from './component/NameList';
+import ParentComponent from './component/ParentComponent';
+import Stae from './component/Stae';
+import UserGreeting from './component/UserGreeting';
+import Tipping from './component/practice/Tipping';
+
+
 
 
 
@@ -18,7 +18,7 @@ import Form from './component/Form';
 function App() {
   return (
     <div className="App">
-      {/*<Greeting  name="Nigeria" hero="giant of africa"/>
+      <Greeting  name="Nigeria" hero="giant of africa"/>
       <Greeting  name="united state" hero=" giant of america"/>
       <Greeting  name="russia" hero="giant of europe"/>
       <Stae/>
@@ -28,8 +28,8 @@ function App() {
       <ParentComponent/>
       <Tipping/>
       <UserGreeting/>
-  <NameList/>
-      <StyleSheet primary={true}/> */}
+      <LifecycleA/>
+      <NameList/>
       <Form/>
     </div>
   );
--- leatr/src/component/Form.js
+++ leatr/src/component/Form.js
@@ -1,15 +1,60 @@
- import React from 'react'
- 
- const Form = () => {
-   return (
-     <Form>
-        <div>
-            <label>Username</label>
-            <input type='text'/>
-        </div>
-     </Form>
-   )
- }
- 
- export default Form
- 
+import React, { useState } from 'react'
+
+const Form = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        Comment: '',
+        topic: 'react'
+    })
+
+    const handleUsernameChange = (e) => {
+        setFormData({
+            ...formData,
+            username: e.target.value
+        });
+    }
+
+    const handleCommentChange = (e) => {
+        setFormData({
+            ...formData,
+            Comment: e.target.value
+        });
+    }
+
+    const handleTopicChange = (e) => {
+        setFormData({
+            ...formData,
+            topic: e.target.value
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`${formData.username} ${formData.Comment} ${formData.topic}`);
+      };
+
+  return (
+    <form onSubmit={handleSubmit}>
+    <div>
+        <label>Username</label>
+        <input type='text' value={formData.username} onChange={handleUsernameChange}/>
+    </div>
+    <div>
+        <label>Comment</label>
+        <textarea value={formData.Comment} onChange={handleCommentChange}/>
+    </div>
+    <div>
+        <label>Topic</label>
+        <select value={formData.topic} onChange={handleTopicChange}>
+            <option value="react">React</option>
+            <option value="vue">Vue</option>
+            <option value="angular">Angular</option>
+        </select>
+    </div> 
+
+    <button type='submit' >Submit</button>
+  </form>
+  )
+}
+
+export default Form
